90dd44a24b2c871901b285b896dbb7cee0828e58
Kit Langton <kitlangton@gmail.com>
2016-01-14 13:17:08 -0500

    quicksort

--- benchmark.rb
+++ benchmark.rb
@@ -1,31 +1,35 @@
 require_relative 'merge_sort'
 require_relative 'bubble_sort'
 require_relative 'insertion_sort'
+require_relative 'quick_sort'
 
 
 def sort_benchmark
 
   start = Time.now
-  50.times do
-    yield
-  end
+  yield
   finish = Time.now
 
   puts (finish - start)
 end
-array = Array.new(7000){rand(3000)}
+array = Array.new(2000){rand(3000)}
 
 puts "insertion sort"
 sort_benchmark do
   insertion_sort(array)
 end
 
-puts "bubble sort"
-sort_benchmark do
-  bubble_sort(array)
-end
+# puts "bubble sort"
+# sort_benchmark do
+#   bubble_sort(array)
+# end
 
 puts "merge sort"
 sort_benchmark do
   merge_sort(array)
+end
+
+puts "quick sort"
+sort_benchmark do
+  quick_sort(array, 0, array.length-1)
 end
--- quick_sort.rb
+++ quick_sort.rb
@@ -1,26 +1,26 @@
 def partition(array, low, high)
-
   pivot = array[high]
 
   (low...high).each do |j|
-
-    p array[low..j]
-    p array[j..high]
-
     if array[j] < pivot
-      low +=1
       array[low], array[j] = array[j], array[low]
+      low +=1
     end
   end
-  array[low+1], array[high] = array[high], array[low+1]
+
+  array[low], array[high] = array[high], array[low]
+  low
 end
 
-array = [12,7,14,9,10,11]
-partition(array, 0, 5)
+def quick_sort(array, p, r)
+  if p < r
+    part = partition(array,p,r)
+    quick_sort(array, p, part-1)
+    quick_sort(array, part+1, r)
+  end
+end
 
+array = [12,7,14,9,10,11, 1283, 87,87 ,87 ,87 ,6]
+quick_sort(array, 0, array.length-1)
 p array
-
-
-# def quick_sort(array)
-
-# end
+# >> [6, 7, 9, 10, 11, 12, 14, 87, 87, 87, 87, 1283]
